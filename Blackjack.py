d73fb5685462eb7971d985f33753074a1ce3e6e6
Daniel <daniel@dljurek.com>
2021-04-03 23:10:16 -0700

    out with the old, in with the new.

--- Blackjack.py
+++ Blackjack.py
@@ -1,35 +1,24 @@
-# Milestone Project 2 - Blackjack Game
-# Here are the requirements:
-
-# You need to create a simple text-based BlackJack game
-# The game needs to have one player versus an automated dealer.
-# The player can stand or hit.
-# The player must be able to pick their betting amount.
-# You need to keep track of the player's total money.
-# You need to alert the player of wins, losses, or busts, etc...
-# And most importantly:
-
-# You must use OOP and classes in some portion of your game. 
-# You can not just use functions in your game. 
-# Use classes to help you define the Deck and the Player's hand. 
-
-# Try including multiple players.
-# Try adding in Double-Down and card splits!
-
 import cards
 import sys
+import time
+
+# initial setup; make the player, dealer, deck, and pot
+player = cards.Player()
+dealer = cards.Player("Dealer")
+dealer_deck = cards.Deck()
+pot = cards.Pot()
 
 def show_table():
     print('')
     print('***********************')
     print('')
-    print('Dealer hand:')
+    print("Dealer's hand:")
     for dealer_card in dealer.all_cards:
         print(f"({dealer_card.value}) > {dealer_card}")
     print(f"[{dealer.hand_value()}]")
     print('')
     print('')
-    print('Player hand:')
+    print(f"{player.name}'s hand:")
     for player_card in player.all_cards:
         print(f"({player_card.value}) > {player_card}")
     print(f"[{player.hand_value()}]")
@@ -43,12 +32,6 @@ def initial_deal():
     player.all_cards.append(dealer_deck.deal_card())
     dealer.all_cards.append(dealer_deck.deal_card())
 
-def make_bet(bet):
-    player.deduct(bet)
-    pot.add(bet*2)
-    print(f"{player.name} bets {bet} dollars.")
-    print(f"Pot contains {pot.amount} dollars.")
-
 def next_move():
     option = int(input('''
     What do you want to do next?
@@ -56,87 +39,49 @@ def next_move():
     2. Hit
     3. Quit
     '''))
-    
-    # if stand
-    if option == 1: 
-    #     while dealer.total NOT in range 17-21
-        while dealer.hand_value() not in range(16,22):
-    #         dealer draws card
-            dealer.all_cards.append(dealer_deck.deal_card())
-    #         show the table
-            show_table()
-    #     compare dealer score to player score
-    #         if dealer score > player score 
-            if dealer.hand_value() > player.hand_value():
-    #             dealer wins
-                print("Dealer wins!")
-    #         else
-            elif dealer.hand_value() < player.hand_value():
-    #           player wins
-                print("Player wins!")
-            else:
-                print("Push!")
-    # elif hit
-    elif option == 2:
-    #     player draws card
-        player.all_cards.append(dealer_deck.deal_card())
-    #     if player total > 21
-        if player.hand_value() > 21:
-    #         dealer wins
-            print("Dealer wins!")
-            return
-        elif player.hand_value() == 21:
-            print("Player wins!")
-            # check dealer hand for push
-            return
-        else:
-            show_table()
-            next_move()
 
-    elif option == 3:
+    if option == 1: # stand
+        while dealer.hand_value() < 17:
+            dealer.all_cards.append(dealer_deck.deal_card())
+            show_table()
+            time.sleep(1)
+            continue
+        
+        if dealer.hand_value() >= 17:
+            who_wins()
+
+    elif option == 2: # hit
+        player.all_cards.append(dealer_deck.deal_card())
+        show_table()
+        if player.hand_value() > 21:
+            print("BUST!!")
+            return
+        next_move()
+        
+    elif option == 3: # quit
         sys.exit(0)
 
-def check_for_win(deal_num):
-    if deal_num == 1:
-        if dealer.hand_value() == 21 and player.hand_value() != 21:
-            print("Dealer wins!")
-        elif dealer.hand_value() > 21 and player.hand_value() < 21:
-            print("Player wins!")
-        elif player.hand_value() > 21:
-            print("Dealer wins!")
+def who_wins():
+    if dealer.hand_value() > 21:
+        show_table()
+        print("Player wins!")
+        return
 
-def payout(amount):
-    player.add(pot.amount)
-    print(f"{player.name} wins {pot.amount}!")
-    pot.deduct(pot.amount)
+    if dealer.hand_value() > player.hand_value():
+        show_table()
+        print("Dealer wins!")
+        return
 
-def reset_hands():
-    player.all_cards.clear()
-    dealer.all_cards.clear()
+    elif dealer.hand_value() < player.hand_value():
+        show_table()
+        print(f"{player.name} wins!")
+        return
 
-def play_round():
-    reset_hands()
-    # Take bet
-    bet = int(input(f"Choose an amount to bet ({player.money} max): "))
-    # remove bet from player and put it in pot.
-    make_bet(bet)
-    # deal cards
-    initial_deal()
-    show_table()
-    next_move()
+    elif dealer.hand_value() == player.hand_value():
+        show_table()
+        print("Push")
+        return
 
-
-# create player and dealer
-player_name = input("Who is playing? ")
-player = cards.Player(player_name)
-dealer = cards.Player("Dealer")
-game_over = False
-dealer_deck = cards.Deck()
-pot = cards.Pot()
-deal_num = 1
-print(f"Welcome, {player.name}! Let's play Blackjack!")
-
-
-# Start the game
-while game_over == False:
-    play_round()
+initial_deal()
+show_table()
+next_move()
